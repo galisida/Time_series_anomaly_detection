d9bae569fae6e684f5b6b7d781c5593ba59dd63e
cocoshe <1228759711@qq.com>
2021-12-09 12:05:39 +0800

    feat: params choices

--- eval.py
+++ eval.py
@@ -1,3 +1,4 @@
+import argparse
 import math
 import time
 import torch
@@ -10,57 +11,74 @@ from utils.plot_and_loss import plot_and_loss
 from utils.reconstruct import predict_future
 from utils.eval import evaluate
 
-torch.manual_seed(0)
-np.random.seed(0)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train.')
+    parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
+    parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
+    parser.add_argument('--dropout', type=float, default=0.1, help='Dropout rate.')
+    parser.add_argument('--input_window', type=int, default=100, help='Number of input steps.')
+    parser.add_argument('--output_window', type=int, default=1, help='Number of prediction steps, '
+                                                                     'in this model its fixed to one.')
+    parser.add_argument('--batch_size', type=int, default=10, help='Number of batch_size.')
+    parser.add_argument('--model', type=str, default="weights/best_model.pth", help='Load model path.(default: '
+                                                                                    'weights/best_model.pth)')
+    # parser.add_argument("--config", help="train config file path")
+    # parser.add_argument("--seed", type=int, default=None, help="random seed")
+    return parser.parse_args()
+
+
+# torch.manual_seed(0)
+# np.random.seed(0)
 
 # S is the source sequence length
 # T is the target sequence length
 # N is the batch size
 # E is the feature number
 
-#src = torch.rand((10, 32, 512)) # (S,N,E)
-#tgt = torch.rand((20, 32, 512)) # (T,N,E)
-#out = transformer_model(src, tgt)
+# src = torch.rand((10, 32, 512)) # (S,N,E)
+# tgt = torch.rand((20, 32, 512)) # (T,N,E)
+# out = transformer_model(src, tgt)
+
+weight_path = "weights/best_model"
 
 
+def main(args):
+    input_window = 100  # number of input steps
+    output_window = 1  # number of prediction steps, in this model its fixed to one
+    batch_size = 10
+    weight_path = args.model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-input_window = 100  # number of input steps
-output_window = 1  # number of prediction steps, in this model its fixed to one
-batch_size = 10
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = TransAm().to(device).load_state_dict(torch.load(weight_path))
+    train_data, val_data = get_data(input_window, output_window, device=device)
 
-train_data, val_data = get_data(input_window, output_window, device=device)
-model = TransAm().to(device)
+    criterion = nn.MSELoss()
 
-criterion = nn.MSELoss()
+    epochs = 1  # The number of epochs
 
-epochs = 1  # The number of epochs
+    epoch_start_time = time.time()
 
-epoch_start_time = time.time()
+    val_loss = plot_and_loss(model, val_data, 1, criterion, input_window)
+    predict_future(model, val_data, 200, input_window)
+    val_loss = evaluate(model, val_data, criterion, input_window)
 
-val_loss = plot_and_loss(model, val_data, 1, criterion, input_window)
-predict_future(model, val_data, 200, input_window)
-val_loss = evaluate(model, val_data, criterion, input_window)
-
-print('-' * 89)
-print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.5f} | valid ppl {:8.2f}'
-      .format(1, (time.time() - epoch_start_time), val_loss, math.exp(val_loss)))
-print('-' * 89)
+    print('-' * 89)
+    print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.5f} | valid ppl {:8.2f}'
+          .format(1, (time.time() - epoch_start_time), val_loss, math.exp(val_loss)))
+    print('-' * 89)
 
     # if val_loss < best_val_loss:
     #    best_val_loss = val_loss
     #    best_model = model
 
-
-# src = torch.rand(input_window, batch_size, 1) # (source sequence length,batch size,feature number)
-# out = model(src)
-#
-# print(out)
-# print(out.shape)
-
+    # src = torch.rand(input_window, batch_size, 1) # (source sequence length,batch size,feature number)
+    # out = model(src)
+    #
+    # print(out)
+    # print(out.shape)
 
 
-
-
-
-
+if __name__ == "__main__":
+    args = parse_args()
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import os
 import torch
 import torch.nn as nn
 import numpy as np
+import argparse
 from utils.data_prepare import get_data
 from model.Transformer import TransAm
 from utils.train import train
@@ -11,8 +12,27 @@ from utils.plot_and_loss import plot_and_loss
 from utils.reconstruct import predict_future
 from utils.eval import evaluate
 
-torch.manual_seed(0)
-np.random.seed(0)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train.')
+    parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
+    parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
+    parser.add_argument('--dropout', type=float, default=0.1, help='Dropout rate.')
+    parser.add_argument('--input_window', type=int, default=100, help='Number of input steps.')
+    parser.add_argument('--output_window', type=int, default=1, help='Number of prediction steps, '
+                                                                     'in this model its fixed to one.')
+    parser.add_argument('--batch_size', type=int, default=10, help='Number of batch_size.')
+    parser.add_argument('--model', type=str, default="weights/best_model.pth", help='Load model path.(default: '
+                                                                                    'weights/best_model.pth)')
+    # parser.add_argument("--config", help="train config file path")
+    # parser.add_argument("--seed", type=int, default=None, help="random seed")
+    return parser.parse_args()
+
+
+
+# torch.manual_seed(0)
+# np.random.seed(0)
 
 if not os.path.exists("weights"):
     os.mkdir("weights")
@@ -31,57 +51,69 @@ if not os.path.exists("weights/" + present):
 # tgt = torch.rand((20, 32, 512)) # (T,N,E)
 # out = transformer_model(src, tgt)
 
-input_window = 100  # number of input steps
-output_window = 1  # number of prediction steps, in this model its fixed to one
-batch_size = 10
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-train_data, val_data = get_data(input_window, output_window, device=device)
-model = TransAm().to(device)
+def main(args):
+    # input_window = 100  # number of input steps
+    # output_window = 1  # number of prediction steps, in this model its fixed to one
+    # batch_size = 10
+    input_window = args.input_window
+    output_window = args.output_window
+    batch_size = args.batch_size
+    epochs = args.epochs
+    lr = args.lr
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-criterion = nn.MSELoss()
-lr = 0.005
-# optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+    train_data, val_data = get_data(input_window, output_window, device=device)
+    model = TransAm().to(device)
 
-best_val_loss = float("inf")
-epochs = 100  # The number of epochs
-best_model = None
+    criterion = nn.MSELoss()
+    # lr = 0.005
+    # optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
 
-for epoch in range(1, epochs + 1):
-    epoch_start_time = time.time()
-    train(train_data, input_window, model, optimizer, criterion, scheduler)  # , epoch=100, batch_size=10)
+    best_val_loss = float("inf")
+    # epochs = 100  # The number of epochs
+    best_model = None
 
-    if epoch % 1 == 0:
-        val_loss = plot_and_loss(model, val_data, epoch, criterion, input_window)
-        predict_future(model, val_data, 200, input_window)
-        save_path = "weights/" + present + "/trained-for-" + str(epoch) + "-ecpoch.pth"
-        torch.save(model.state_dict(), save_path)
-    else:
-        val_loss = evaluate(model, val_data, criterion, input_window)
+    for epoch in range(1, epochs + 1):
+        epoch_start_time = time.time()
+        train(train_data, input_window, model, optimizer, criterion, scheduler, epoch, batch_size)
 
-    print('-' * 89)
-    print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.5f} | valid ppl {:8.2f}'.format(epoch, (
-            time.time() - epoch_start_time), math.exp(val_loss)))
-    print('-' * 89)
+        if epoch % 1 == 0:
+            val_loss = plot_and_loss(model, val_data, epoch, criterion, input_window)
+            predict_future(model, val_data, 200, input_window)
+            save_path = "weights/" + present + "/trained-for-" + str(epoch) + "-ecpoch.pth"
+            torch.save(model.state_dict(), save_path)
+        else:
+            val_loss = evaluate(model, val_data, criterion, input_window)
 
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss
-        best_model = model
-        save_path = "weights/best_model.pth"
-        torch.save(model.state_dict(), save_path)
-        # print("save successfully")
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.5f} | valid ppl {:8.2f}'.format(epoch, (
+                time.time() - epoch_start_time), math.exp(val_loss)))
+        print('-' * 89)
 
-    scheduler.step()
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model = model
+            save_path = "weights/best_model.pth"
+            torch.save(model.state_dict(), save_path)
+            # print("save successfully")
 
-# src = torch.rand(input_window, batch_size, 1) # (source sequence length,batch size,feature number)
-# out = model(src)
-#
-# print(out)
-# print(out.shape)
+        scheduler.step()
+
+    # src = torch.rand(input_window, batch_size, 1) # (source sequence length,batch size,feature number)
+    # out = model(src)
+    #
+    # print(out)
+    # print(out.shape)
 
 
-# save_path = "weights/last_model.pth"
-# torch.save(model.state_dict(), save_path)
-# print("save successfully")
+    # save_path = "weights/last_model.pth"
+    # torch.save(model.state_dict(), save_path)
+    # print("save successfully")
+
+if __name__ == "__main__":
+    args = parse_args()
+    # print(args)
+    main(args)
